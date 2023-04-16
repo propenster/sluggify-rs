a124f30a1bfd58a2a3ca133ed86e54496cbeee19
propenster <faitholusegun60@gmail.com>
2023-04-16 16:25:36 +0100

    typo in year

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 20236 propenster.
+// Copyright 2023 propenster.
 // Licensed under the MIT license (http://opensource.org/licenses/MIT)
 // This file may not be copied, modified, or distributed
 // except according to those terms.
