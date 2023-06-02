3ff4814df230649d91b87ddad6e5fad0134e2e5b
propenster <faitholusegun60@gmail.com>
2023-06-02 11:29:38 +0100

    refactor, make more idiomatic rust code

--- src/sluggify/mod.rs
+++ src/sluggify/mod.rs
@@ -1,4 +1,7 @@
-use regex::Regex;
+use regex::{Regex};
+//use std::ops::Deref;
+
+
 
 /// Use this struct to make customizations for your Slugify helper
 /// You can use it to configure bad characters , or set the Slugify helper to collapse whitespaces or not.
@@ -39,9 +42,7 @@ impl SluggifyOptions{
 /// let expected = String::from("this-is-a-very-long-snake");
 /// let output = sluggify("This is a very long snake", None);
 /// assert_eq!(output, expected);
-pub fn sluggify(input: &str, sluggify_options: Option<SluggifyOptions>) -> String {
-    //println!("In the sluggify function... The sluggify option struct {:?}", sluggify_options);
-    
+pub fn sluggify(input: &str, sluggify_options: Option<SluggifyOptions>) -> String {    
     let mut result_string = String::from("");
     let mut final_input = String::from("");
     let mut collapse_space: bool = false;
@@ -50,80 +51,68 @@ pub fn sluggify(input: &str, sluggify_options: Option<SluggifyOptions>) -> Strin
     match sluggify_options {
         Some(b) => {
             collapse_space = b.collapse_whitespace;
-            //println!("Collapsed spaces +++ {}", collapse_space);
             disallowed_chars = b.disallowed_characters;
-            //println!("Disallowed.... +++ {:?}", &disallowed_chars);
             use_default_config = false;
-            //println!("Use default config {}", use_default_config);
-
         },
         None => {
             use_default_config = true;
             collapse_space = true;
             disallowed_chars = SluggifyOptions::new().disallowed_characters;
-            //println!("Use default config {}", use_default_config);
         }
         
     };
 
-    // println!("Collapsed spaces....  +++ {}", collapse_space);
-    //         println!("Disallowed.... +++ {:?}", &disallowed_chars);
-    
-    
     if use_default_config{
         let cfg = SluggifyOptions::new();
         collapse_space = cfg.collapse_whitespace;
 
         for c in input.chars() {
-            if !cfg.disallowed_characters.contains(&c){
-                final_input.push(c); 
-            }else {
-                final_input.push(' ');
+            if !cfg.disallowed_characters.contains(&c) {
+                //final_input.push(c); 
+                final_input = format!("{}{}", final_input, c);
+
+            } else {
+                //final_input.push(' ');
+                final_input = format!("{}{}", final_input, c);
+
             }
         }
 
     }else{
         for c in input.chars() {
-            if !disallowed_chars.contains(&c){
-                final_input.push(c); 
-            }else{
-                final_input.push(' ');
+            if !disallowed_chars.contains(&c) {
+                //final_input.push(c); 
+                final_input = format!("{}{}", final_input, c);
+
+            } else {
+                //final_input.push(' ');
+                final_input = format!("{}{}", final_input, ' ');
+
             }
         }
 
     }
 
-    //we have still not handled collapse whitespaces...
-    //println!("Final Input after processing through disallowed characters {}", &final_input);
-    
+    //we have still not handled collapse whitespaces...    
     if !collapse_space{
         //regex that leaves spaces or replace with -
         result_string = do_regex(&final_input, false);
-        //println!("Result String of Input {} is {}", &final_input, &result_string);
 
     }else{
         //our normal regex...
         result_string = do_regex(&final_input, true);
-        //println!("Result String of Input {} is {}", &final_input, &result_string);
     }
 
-    //println!("Result string for input {} is = {}",&input, &result_string);
     return result_string;
     
 }
 
 fn do_regex(input: &str, collapse_spaces: bool) -> String {
-    let mut pattern: &str = "";
-
-    let mut re: Regex = Regex::new("^/d{5}").unwrap();
-
-    if collapse_spaces == true {
-        re = Regex::new(r"[^a-zA-Z0-9]+").unwrap();
-    }else {
-        re = Regex::new(r"\W").unwrap();
-    }
-    //println!("Patter for this input {} is{}", &input, &pattern);
-    let output = re.replace_all(input, "-").to_string();
+    let mut re = match collapse_spaces {
+        true => Regex::new(r"[^a-zA-Z0-9]+"),
+        false => Regex::new(r"\W"),
+    };
+    let output = re.unwrap().replace_all(input, "-").to_string();
     output.trim_matches('-').to_lowercase()
 }
 
